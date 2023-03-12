4390ecbf58b19dcf9046b05111d46b85845a051c
TatevSanturyan <126407466+TatevSanturyan@users.noreply.github.com>
2023-03-12 00:23:30 -0800

    Update myproject.py

--- myproject.py
+++ myproject.py
@@ -1,24 +1,5 @@
-class Bird:
-    ruClassName = "Птица"
-    objInstancesCount = 0
+n = [4,5,7,7,4,5]
+dub = {i for i in n if n.count(i)>1}
+print(dub)
 
-    def __init__(self, name: str, id: int, age: int):
-        self.__name = name
-        self.id = id
-        self.age = age
-        Bird.objInstancesCount = Bird.objInstancesCount + 1
-    def get_name(self):
-        return self.__name
 
-    def set_name(self, name):
-        self.__name = name
-    def info(self):
-
-        print(self.__name)
-        print("id :" + str(self.id))
-        print("age:" + str(self.age))
-
-b1 = Bird(name = "объект №1 класса " + Bird.ruClassName, id = 123456, age=15)
-print("its " + b1.get_name())
-b1.set_name("goose")
-print("its " + b1.get_name() )
